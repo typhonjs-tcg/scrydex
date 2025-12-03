ce94f3c0a475c7017c5968c21078d4cc81028798
typhonrt <support@typhonjs.io>
2025-12-02 20:41:29 -0800

    Add supported formats data.

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,4 @@
 export * from './csv/Collection.js';
 export * from './csv/ImportedIndex.js';
 export * from './sort/SortedFormat.js';
+export * from './scryfall/index.js';
---
+++ src/data/scryfall/supportedFormats.js
@@ -0,0 +1,10 @@
+/**
+ * Supported Scryfall formats for legality checks.
+ *
+ * @type {ReadonlySet<string>}
+ */
+const supportedFormats = Object.freeze(new Set(['standard', 'future', 'historic', 'timeless', 'gladiator', 'pioneer',
+ 'modern', 'legacy', 'pauper', 'vintage', 'penny', 'commander', 'oathbreaker', 'standardbrawl', 'brawl', 'alchemy',
+  'paupercommander', 'duel', 'oldschool', 'premodern', 'predh']));
+
+export { supportedFormats };
