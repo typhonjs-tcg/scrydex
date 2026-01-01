8ea98657a6241bfceb0cdd9b7a30e60156202e58
typhonrt <support@typhonjs.io>
2026-01-01 03:51:09 -0800

    Simplified `exportCards` helper.

--- src/commands/export/common/exportCards.ts
+++ src/commands/export/common/exportCards.ts
@@ -22,38 +22,17 @@ export async function* exportCards({ db, coalesce = true }: { db: CardStream, co
 
       // First pass - calculate quantity of unique card entries ------------------------------------------------------
 
-      const uniqueKeyMap = new Map<string, number>();
-
-      for await (const card of db.asStream({ isExportable: true }))
-      {
-         if (typeof card.quantity !== 'number' || !Number.isInteger(card.quantity) || card.quantity <= 0)
-         {
-            logger.warn(`Skipping card (${card.name}) from '${db.meta.name}' due to invalid quantity: ${
-             card.quantity}`);
-
-            continue;
-         }
-
-         const uniqueKey = uniqueCardKey(card);
-
-         const quantity = uniqueKeyMap.get(uniqueKey);
-         uniqueKeyMap.set(uniqueKey, typeof quantity === 'number' ? quantity + card.quantity : card.quantity);
-      }
+      const uniqueKeyMap = await db.getQuantityMap({ isExportable: true, logger });
 
       // Second pass exporting unique card entries -------------------------------------------------------------------
 
-      for await (const card of db.asStream({ isExportable: true }))
+      for await (const card of db.asStream({ uniqueKeys: uniqueKeyMap, uniqueOnce: true }))
       {
-         const uniqueKey = uniqueCardKey(card);
-
-         const quantity = uniqueKeyMap.get(uniqueKey);
+         const quantity = uniqueKeyMap.get(uniqueCardKey(card));
          if (typeof quantity === 'number')
          {
             card.quantity = quantity;
-
             yield card;
-
-            uniqueKeyMap.delete(uniqueKey);
          }
       }
    }
