7b9d0ca440253b77c13a4d1e7f1a9e01a127625e
typhonrt <support@typhonjs.io>
2026-02-20 06:54:34 -0800

    Update config.

---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+import {
+   configDefaults,
+   defineConfig } from 'vitest/config';
+
+export default defineConfig({
+   test: {
+      exclude: [...configDefaults.exclude],
+      include: ['./test/src/**/*.test.ts'],
+      coverage: {
+         include: ['src/**'],
+         exclude: ['test/**'],
+         provider: 'v8',
+         reporter: ['text', 'json', 'html']
+      },
+      reporters: ['default', 'html'],
+      globals: true,
+      testTimeout: 40000,
+      // server: {
+      //    watch: {
+      //       ignored: ['./test/fixture/**/*']
+      //    }
+      // }
+   }
+});
