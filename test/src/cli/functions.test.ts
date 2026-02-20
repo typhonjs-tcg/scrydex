421f7b7b0917eb4f7c7478bce0e0b077311f6c0e
typhonrt <support@typhonjs.io>
2026-02-20 06:47:32 -0800

    Initial Vitest tests.

---
+++ test/src/cli/functions.test.ts
@@ -0,0 +1,92 @@
+import fs               from 'node:fs';
+
+import {
+   assert,
+   expect,
+   vi }                 from 'vitest';
+
+import { CardDB }       from '#scrydex/data/db';
+
+import {
+   commandConvertCsv,
+   commandDiff,
+   commandExportCsv,
+   commandExportExcel,
+   commandExportLLM,
+   commandExportTxt,
+   commandFileCompress,
+   commandFilter,
+   commandFind,
+   commandScryfallDownload,
+   commandSortFormat }  from '../../../src/cli/functions';
+
+
+describe('CLI Command:', () =>
+{
+   describe('convert-csv (commandConvertCsv)', () =>
+   {
+      it('compressed', async () => {
+         await commandConvertCsv('./test/fixture/input/csv/manabox/collection', {
+            db: './test/fixture/input/db/scryfall_test_cards.json.gz',
+            output: './test/fixture/output/cli/convert-csv/inventory.json',
+            loglevel: 'error',
+            compress: true,
+            'group-external': './test/fixture/input/csv/manabox/groups/group-decks.csv',
+            'group-decks': './test/fixture/input/csv/manabox/groups/group-external.csv',
+            'group-proxy': './test/fixture/input/csv/manabox/groups/group-proxy.csv',
+         });
+
+         const result = await CardDB.load({ filepath: './test/fixture/output/cli/convert-csv/inventory.json.gz' });
+         const snapshot = await CardDB.load({ filepath: './test/fixture/snapshot/cli/convert-csv/inventory.json.gz' });
+
+         assert.isDefined(result);
+         assert.isDefined(snapshot);
+
+         const cardsResult = await result.getAll();
+         const cardsSnapshot = await snapshot.getAll();
+
+         assert.isArray(cardsResult);
+         assert.isArray(cardsSnapshot);
+
+         assert.deepEqual(cardsResult, cardsSnapshot);
+      });
+
+      it('uncompressed', async () => {
+         await commandConvertCsv('./test/fixture/input/csv/manabox/collection', {
+            db: './test/fixture/input/db/scryfall_test_cards.json.gz',
+            output: './test/fixture/output/cli/convert-csv/inventory.json',
+            loglevel: 'error',
+            'group-external': './test/fixture/input/csv/manabox/groups/group-decks.csv',
+            'group-decks': './test/fixture/input/csv/manabox/groups/group-external.csv',
+            'group-proxy': './test/fixture/input/csv/manabox/groups/group-proxy.csv',
+         });
+
+         const result = await CardDB.load({ filepath: './test/fixture/output/cli/convert-csv/inventory.json' });
+         const snapshot = await CardDB.load({ filepath: './test/fixture/snapshot/cli/convert-csv/inventory.json' });
+
+         const cardsResult = await result.getAll();
+         const cardsSnapshot = await snapshot.getAll();
+
+         assert.isArray(cardsResult);
+         assert.isArray(cardsSnapshot);
+
+         assert.deepEqual(cardsResult, cardsSnapshot);
+      });
+
+      it('not found (DB missing card)', async () => {
+         const consoleLog: any[] = [];
+         vi.spyOn(console, 'log').mockImplementation((...args) => consoleLog.push(args));
+
+         await commandConvertCsv('./test/fixture/input/csv/manabox/other-tests/not-found.csv', {
+            db: './test/fixture/input/db/scryfall_test_cards.json.gz',
+            output: './test/fixture/output/cli/convert-csv/not-found.json',
+            loglevel: 'warn'
+         });
+
+         vi.restoreAllMocks();
+
+         assert.equal(JSON.stringify(consoleLog),
+          fs.readFileSync('./test/fixture/snapshot/cli/convert-csv/not-found-log.txt', 'utf-8'));
+      });
+   });
+});
