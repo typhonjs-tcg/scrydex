0d298ddced28cdceb86d5f6c18f35ce2e2e093e3
typhonrt <support@typhonjs.io>
2026-02-23 21:15:00 -0800

    `export-llm` tests.

--- test/src/cli/functions.test.ts
+++ test/src/cli/functions.test.ts
@@ -132,6 +132,45 @@ describe('CLI Commands:', () =>
       });
    });
 
+   describe('export-llm', () =>
+   {
+      it('default (collection)', async () =>
+      {
+         await commandExportLLM('./test/fixture/snapshot/cli/sort-format/collection', {
+            output: './test/fixture/output/cli/export-llm/collection/default',
+            loglevel: 'error'
+         });
+
+         await AssertData.directoryEqual('./test/fixture/output/cli/export-llm/collection/default',
+          './test/fixture/snapshot/cli/export-llm/collection/default');
+      });
+
+      it('no oracle text w/ types (collection)', async () =>
+      {
+         await commandExportLLM('./test/fixture/snapshot/cli/sort-format/collection', {
+            output: './test/fixture/output/cli/export-llm/collection/no-oracle-text',
+            'oracle-text': false,
+            types: true,
+            loglevel: 'error'
+         });
+
+         await AssertData.directoryEqual('./test/fixture/output/cli/export-llm/collection/no-oracle-text',
+          './test/fixture/snapshot/cli/export-llm/collection/no-oracle-text');
+      });
+
+      it('default w/ types (single DB)', async () =>
+      {
+         await commandExportLLM('./test/fixture/snapshot/cli/sort-format/collection/commander/commander.json', {
+            output: './test/fixture/output/cli/export-llm/single/default/llm-commander.json',
+            types: true,
+            loglevel: 'error'
+         });
+
+         await AssertData.directoryEqual('./test/fixture/output/cli/export-llm/single/default',
+          './test/fixture/snapshot/cli/export-llm/single/default');
+      });
+   });
+
    describe('export-txt', () =>
    {
       it('(premodern:oldschool:predh:commander)', async () =>
