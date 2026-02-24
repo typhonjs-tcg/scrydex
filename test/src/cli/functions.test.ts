404622d0ca41a58cc67107c038e931ceed2db5bb
typhonrt <support@typhonjs.io>
2026-02-23 23:36:30 -0800

    Add `export-excel` tests.

--- test/src/cli/functions.test.ts
+++ test/src/cli/functions.test.ts
@@ -132,6 +132,81 @@ describe('CLI Commands:', () =>
       });
    });
 
+   describe('export-excel', () =>
+   {
+      it('default', async () =>
+      {
+         await commandExportExcel('./test/fixture/snapshot/cli/sort-format/collection/commander/commander.json', {
+            output: './test/fixture/output/cli/export-excel/default.xlsx',
+            loglevel: 'error'
+         });
+
+         await AssertData.excelWorkbookFiles('./test/fixture/output/cli/export-excel/default.xlsx',
+          './test/fixture/snapshot/cli/export-excel/default.xlsx');
+      });
+
+      it('by kind', async () =>
+      {
+         await commandExportExcel('./test/fixture/snapshot/cli/sort-format/collection/commander/commander.json', {
+            output: './test/fixture/output/cli/export-excel/by-kind.xlsx',
+            loglevel: 'error',
+            'by-kind': true
+         });
+
+         await AssertData.excelWorkbookFiles('./test/fixture/output/cli/export-excel/by-kind.xlsx',
+          './test/fixture/snapshot/cli/export-excel/by-kind.xlsx');
+      });
+
+      it('by type', async () =>
+      {
+         await commandExportExcel('./test/fixture/snapshot/cli/sort-format/collection/commander/commander.json', {
+            output: './test/fixture/output/cli/export-excel/by-type.xlsx',
+            loglevel: 'error',
+            'by-type': true
+         });
+
+         await AssertData.excelWorkbookFiles('./test/fixture/output/cli/export-excel/by-type.xlsx',
+          './test/fixture/snapshot/cli/export-excel/by-type.xlsx');
+      });
+
+      it('no filename / price', async () =>
+      {
+         await commandExportExcel('./test/fixture/snapshot/cli/sort-format/collection/commander/commander.json', {
+            output: './test/fixture/output/cli/export-excel/no-filename-price.xlsx',
+            loglevel: 'debug',
+            filename: false,
+            price: false
+         });
+
+         await AssertData.excelWorkbookFiles('./test/fixture/output/cli/export-excel/no-filename-price.xlsx',
+          './test/fixture/snapshot/cli/export-excel/no-filename-price.xlsx');
+      });
+
+      it('include rarity', async () =>
+      {
+         await commandExportExcel('./test/fixture/snapshot/cli/sort-format/collection/commander/commander.json', {
+            output: './test/fixture/output/cli/export-excel/include-rarity.xlsx',
+            loglevel: 'error',
+            rarity: true
+         });
+
+         await AssertData.excelWorkbookFiles('./test/fixture/output/cli/export-excel/include-rarity.xlsx',
+          './test/fixture/snapshot/cli/export-excel/include-rarity.xlsx');
+      });
+
+      it('theme dark', async () =>
+      {
+         await commandExportExcel('./test/fixture/snapshot/cli/sort-format/collection/commander/commander.json', {
+            output: './test/fixture/output/cli/export-excel/theme-dark.xlsx',
+            loglevel: 'error',
+            theme: 'dark'
+         });
+
+         await AssertData.excelWorkbookFiles('./test/fixture/output/cli/export-excel/theme-dark.xlsx',
+          './test/fixture/snapshot/cli/export-excel/theme-dark.xlsx');
+      });
+   });
+
    describe('export-llm', () =>
    {
       it('default (collection)', async () =>
