d8698131990049d5f715e4ee3e0c514043708d48
typhonrt <support@typhonrt.org>
2026-03-12 13:57:05 -0700

    Update config

--- vitest.config.ts
+++ vitest.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       },
       reporters: ['default', 'html'],
       globals: true,
-      testTimeout: 40000,
+      testTimeout: 60000,
       globalSetup: ['./test/src/global-setup.ts']
    }
 });
