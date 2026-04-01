c0a71fc3d34dbb3d2505142aafa87f80277f707c
typhonrt <support@typhonjs.io>
2026-03-31 20:49:51 -0700

    Add groups as path snapshot.

--- test/src/cli/commands/convert-csv.test.ts
+++ test/src/cli/commands/convert-csv.test.ts
@@ -38,6 +38,20 @@ describe.runIf(testConfig['convert-csv'])('convert-csv', () =>
        './test/fixture/snapshot/cli/convert-csv/inventory.json');
    });
 
+   it('uncompressed (group concat all)', async () => {
+      // Provide a directory path for `group-decks` combining all group CSV files into the decks group.
+
+      await commandConvertCsv('./test/fixture/input/csv/manabox/collection', {
+         db: './test/fixture/input/db/scryfall_test_cards.json.gz',
+         output: './test/fixture/output/cli/convert-csv/inventory-groups-concat.json',
+         loglevel: 'error',
+         'group-decks': './test/fixture/input/csv/manabox/groups',
+      });
+
+      await AssertData.cardDBFiles('./test/fixture/output/cli/convert-csv/inventory-groups-concat.json',
+       './test/fixture/snapshot/cli/convert-csv/inventory-groups-concat.json');
+   });
+
    it('not found (DB missing card)', async () => {
       const consoleLog: any[] = [];
       vi.spyOn(console, 'log').mockImplementation((...args) => consoleLog.push(args));
